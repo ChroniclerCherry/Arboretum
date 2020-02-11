ae5e8ab528d5bd39930edcd1156392ff05ec5f07
Andy Crocker <atmcrocker@gmail.com>
2020-02-11 17:34:08 +0000

    Add initial project files

---
+++ Arboretum/Arboretum/ModEntry.cs
@@ -0,0 +1,15 @@
+﻿using StardewModdingAPI;
+
+namespace Arboretum
+{
+    /// <summary>Mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /// <summary>Mod entry point.</summary>
+        /// <param name="helper">Provides methods for interacting with the mod directory as well as the modding api.</param>
+        public override void Entry(IModHelper helper)
+        {
+            Monitor.Log("Arboretum loaded");
+        }
+    }
+}
